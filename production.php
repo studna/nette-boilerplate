eb2b5c327795f31317431e1f39ca09ba2d6891e3
studnaa <vojtech.studenka@gmail.com>
2014-06-30 15:36:36 +0200

    Fix production version building script

--- production.php
+++ production.php
@@ -6,6 +6,24 @@ class ExtendedZipArchive extends ZipArchive {
 
   protected $outputFilename;
 
+  protected $movedFoldersCount = 0;
+
+  protected $movedFilesCount = 0;
+
+  protected $moved = Array();
+
+  private function move($filename, $name) {
+    if(!file_exists($filename))
+      return;
+
+    if(isset($this->moved[$filename]))
+      return;
+
+    $this->movedFilesCount++;
+    $this->moved[$filename] = true;  
+    $this->addFromString($name, file_get_contents($filename));
+  }
+
   public function open($filename, $flags = null) {
     $this->outputFilename = $filename;
     return parent::open($filename, $flags);
@@ -13,12 +31,14 @@ class ExtendedZipArchive extends ZipArchive {
 
   public function addDir($location, $name) { 
     if(isset($this->directories[$name]))
-      return;    
+      return;
 
     echo "Moving directory: $name\n\r";
 
+    $this->movedFoldersCount++;
     $this->directories[$name] = true;
-    $this->addDirFiles($location, $name); 
+
+    return $this->addDirFiles($location, $name); 
   }
 
   public function cleanDir($name) {
@@ -28,7 +48,7 @@ class ExtendedZipArchive extends ZipArchive {
   }
 
   public function close() {    
-
+    echo "\nMoved {$this->movedFilesCount} files and {$this->movedFoldersCount} folders.\n";
     echo "\nProduction file \"" . basename($this->outputFilename) ."\" was generated.\n"; 
     return parent::close();
   }
@@ -36,28 +56,32 @@ class ExtendedZipArchive extends ZipArchive {
   private function addDirFiles($location, $name) {       
       $location = realpath($location);
       $location = str_replace('\\', '/', $location);
-      $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($location), RecursiveIteratorIterator::SELF_FIRST);
+      
+      $recursiveDirectoryIterator = new RecursiveDirectoryIterator($location, 
+        RecursiveDirectoryIterator::SKIP_DOTS);
+
+      $files = new RecursiveIteratorIterator($recursiveDirectoryIterator, 
+        RecursiveIteratorIterator::SELF_FIRST);
       
       foreach($files as $file) {
         
         $filePath = realpath($file);
-        $filePath = str_replace('\\', '/', $filePath);
+        $filePath = str_replace('\\', '/', $filePath);        
 
         $parts = explode($location, $filePath);
                 
         if(!isset($parts[1]) || empty($parts[1]))
          continue;
 
-       $relativePath = $name . $parts[1];
+        
+        $relativePath = $name . $parts[1];
 
         if(is_dir($filePath)) {                             
           $this->addEmptyDir($relativePath);
           $this->addDir($filePath, $relativePath);          
-        } else {
-          $filename = $name . "/" . basename($filePath);  
-          
-          if(file_exists(__DIR__ . "/" . $filename))
-            $this->addFromString($filename, file_get_contents($filePath));
+        } else {          
+          $filename = $relativePath;  
+          $this->move($filePath, $filename);          
         }
       }
       
@@ -77,4 +101,6 @@ $archive->addDir(__DIR__ . "/temp", "temp");
 $archive->cleanDir("temp/cache");
 $archive->addDir(__DIR__ . "/vendor", "vendor");
 $archive->addDir(__DIR__ . "/public", "public");
+$archive->addFile(".htaccess", ".htaccess");
+$archive->addFile(".webconfig", ".webconfig");
 $archive->close();    
